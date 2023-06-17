d4267b300868d3b35e085e6d107c62cb5fece14d
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2023-06-16 23:38:21 -0300

    js-perguntas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
-
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
@@ -12,9 +11,7 @@ function ativarLink(link) {
 links.forEach(ativarLink);
 
 // Ativar Items do orçamento
-
 const parametros = new URLSearchParams(location.search);
-
 function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
 
@@ -22,5 +19,23 @@ function ativarProduto(parametro) {
     elemento.checked = true;
   }
 }
-
 parametros.forEach(ativarProduto);
+
+// Perguntas
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
